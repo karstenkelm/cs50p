eecf7019be20275f47e27650cca594505b60710a
karsten kelm <karstenkelm@gmail.com>
2023-12-06 16:40:10 -0800

    6. Dezember

---
+++ week_1/compare.py
@@ -0,0 +1,13 @@
+# Edited on 20231109 8:56h
+
+x = int(input("Whats X? "))
+y = int(input("Whats Y? "))
+
+if x < y:
+    print("X is less than Y")
+
+elif x > y:
+    print("X is greater than Y")
+
+else:
+    print("X is equal to Y")
